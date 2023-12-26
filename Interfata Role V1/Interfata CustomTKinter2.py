1598fa2bd5e917d03d593984d961af5b41eab4bf
Razvan Viorel Silisteanu <razvan@mexi.ro>
2023-12-26 22:01:17 +0200

    2023/12/25 - Interfata CustomTKinter2.py - V1.0 - Functional GUI si DropDown + Update Valori + Titlu fereastra + Comments

--- Interfata Role V1/Interfata CustomTKinter2.py
+++ Interfata Role V1/Interfata CustomTKinter2.py
@@ -3,24 +3,25 @@ import customtkinter as ctk
 from tkinter import ttk, filedialog
 import pandas as pd
 
+# Function to move the window (used for custom title bar dragging)
 def move_window(event):
     root.geometry(f'+{event.x_root}+{event.y_root}')
 
-# Initialize main window
+# Initialize main window without default title bar
 root = ctk.CTk()
 root.geometry('800x600')
 root.overrideredirect(True)  # Turn off the default title bar
 
-# Create a custom title bar
+# Create a custom title bar with a specified height and color
 title_bar = ctk.CTkFrame(root, height=80, fg_color='lightgrey')  # Corrected property name
 title_bar.pack(fill='x')
 title_bar.bind('<B1-Motion>', move_window)
 
-# Custom title label
+# Custom title label with specified text, colors, and font
 title_label = ctk.CTkLabel(title_bar, text="Smecherie by SRV", fg_color='orange', text_color='black', font=('Arial', 16))
 title_label.pack(side='left', padx=10)
 
-# Close button
+# Close button on title bar to close the application
 close_button = ctk.CTkButton(title_bar, text="X", command=root.destroy, fg_color='red', hover_color='dark red')
 close_button.pack(side='right')
 
@@ -32,7 +33,7 @@ file_path = None
 active_tab_color = "Green"
 inactive_tab_color = "Gray"
 
-# Function to switch between tabs and highlight the active tab
+# Function to switch between tabs and update their appearance based on active state
 def switch_tab(tab):
     global tab1_button, tab2_button
     tab1_frame.pack_forget()
@@ -46,7 +47,7 @@ def switch_tab(tab):
         tab1_button.configure(fg_color=inactive_tab_color)
         tab2_button.configure(fg_color=active_tab_color)
 
-# Function to update the Treeview with data based on selected Tambur
+# Function to update the Treeview based on selected 'Tambur' value from dropdown
 def on_dropdown_select(*args):
     global df
     selected_tambur = dropdown_var.get()
@@ -54,13 +55,13 @@ def on_dropdown_select(*args):
         filtered_df = df[(df['Tambur'] == selected_tambur) & (df['KG/Rola'] > 0)]
         update_treeview(filtered_df)
 
-# Function to update the Treeview with data
+# Function to populate the Treeview with data from the DataFrame
 def update_treeview(filtered_df):
     tree.delete(*tree.get_children())
     for index, row in filtered_df.iterrows():
         tree.insert("", tk.END, values=(row['Tambur'], row['KG/Rola'], row['Nr.InternRola']))
 
-# Function to open an Excel file and update dropdown
+# Function to open an Excel file and update dropdown with 'Tambur' values
 def open_excel_file():
     global df, file_path
     file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
@@ -72,7 +73,7 @@ def open_excel_file():
         except Exception as e:
             print("Error opening file:", e)
 
-# Function to create dropdown with new values in the placeholder frame
+# Function to create and position a new dropdown in the placeholder frame
 def create_dropdown(values):
     global dropdown_var, dropdown
 
@@ -86,7 +87,7 @@ def create_dropdown(values):
     dropdown.pack(fill='x', expand=True)
     dropdown_var.trace('w', lambda *args: on_dropdown_select())
 
-# Function to update KG/Rola value in the selected row and save to Excel
+# Function to update a specific 'KG/Rola' value in the DataFrame and save to the Excel file
 def update_kg_rola():
     global df, file_path
     new_kg_rola_value = new_kg_rola_entry.get()
@@ -99,7 +100,7 @@ def update_kg_rola():
         print("Data saved to Excel.")
         new_kg_rola_entry.delete(0, tk.END)  # Clear the entry field
 
-# Create tab frames
+# Create and configure tab frames
 tab1_frame = ctk.CTkFrame(root)
 tab2_frame = ctk.CTkFrame(root)
 
@@ -124,7 +125,7 @@ dropdown_placeholder.pack(pady=10)
 # Create initial empty dropdown in Tab 1 with a placeholder value
 create_dropdown(["Select Tambur"])
 
-# Create entry for new KG/Rola value
+# Create entry field for new KG/Rola value
 new_kg_rola_var = tk.StringVar()
 new_kg_rola_entry = ctk.CTkEntry(tab1_frame, textvariable=new_kg_rola_var, placeholder_text="New KG/Rola")
 new_kg_rola_entry.pack(pady=10)
@@ -133,21 +134,21 @@ new_kg_rola_entry.pack(pady=10)
 update_button = ctk.CTkButton(tab1_frame, text="Update KG/Rola", command=update_kg_rola)
 update_button.pack(pady=10)
 
-# Create Treeview in Tab 1
+# Create Treeview in Tab 1 for displaying data
 tree_columns = ("Tambur", "KG/Rola", "Nr.InternRola")
 tree = ttk.Treeview(tab1_frame, columns=tree_columns, show='headings')
 for col in tree_columns:
     tree.heading(col, text=col)
 tree.pack(expand=True, fill='both', pady=20, padx=20)
 
-# Create input fields in Tab 2
+# Create input fields in Tab 2 for additional functionalities
 for i in range(1, 3):
     entry_var = tk.StringVar()
     entry = ctk.CTkEntry(tab2_frame, textvariable=entry_var, placeholder_text=f"Input {i}")
     entry.pack(pady=10)
 
-# Initially display Tab 1
+# Initially display Tab 1 by default
 switch_tab(1)
 
-# Start the GUI
+# Start the GUI loop
 root.mainloop()
