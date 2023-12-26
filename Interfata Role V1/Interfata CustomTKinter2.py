cd87abdc0e6ba8884dd2d879060aacd63189932d
Razvan Viorel Silisteanu <razvan@mexi.ro>
2023-12-26 18:16:04 +0200

    2023/12/25 - Interfata CustomTKinter2.py - V1.0 - Functional GUI si DropDown

--- Interfata Role V1/Interfata CustomTKinter2.py
+++ Interfata Role V1/Interfata CustomTKinter2.py
@@ -20,28 +20,6 @@ def switch_tab(tab):
     else:
         tab2_frame.pack(fill='both', expand=True)
 
-# Function to recreate dropdown with new values
-def create_dropdown(values):
-    global dropdown_var, dropdown
-    if 'dropdown' in globals():
-        dropdown.destroy()
-    dropdown_var = tk.StringVar()
-    dropdown = ctk.CTkOptionMenu(tab1_frame, variable=dropdown_var, values=values)
-    dropdown.pack(pady=10)
-    dropdown_var.trace('w', lambda *args: on_dropdown_select())
-
-# Function to open an Excel file and update dropdown
-def open_excel_file():
-    global df
-    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
-    if file_path:
-        try:
-            df = pd.read_excel(file_path)
-            tambur_values = df['Tambur'].unique().tolist()
-            create_dropdown(tambur_values)
-        except Exception as e:
-            print("Error opening file:", e)
-
 # Function to update the Treeview with data based on selected Tambur
 def on_dropdown_select(*args):
     global df
@@ -55,6 +33,32 @@ def update_treeview(filtered_df):
     tree.delete(*tree.get_children())
     for index, row in filtered_df.iterrows():
         tree.insert("", tk.END, values=(row['Tambur'], row['KG/Rola'], row['Nr.InternRola']))
+
+# Function to open an Excel file and update dropdown
+def open_excel_file():
+    global df
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
+    if file_path:
+        try:
+            df = pd.read_excel(file_path)
+            tambur_values = df['Tambur'].unique().tolist()
+            create_dropdown(tambur_values)
+        except Exception as e:
+            print("Error opening file:", e)
+
+# Function to create dropdown with new values in the placeholder frame
+def create_dropdown(values):
+    global dropdown_var, dropdown
+
+    # Clear placeholder frame for dropdown
+    for widget in dropdown_placeholder.winfo_children():
+        widget.destroy()
+
+    # Create new dropdown in the placeholder frame
+    dropdown_var = tk.StringVar()
+    dropdown = ctk.CTkOptionMenu(dropdown_placeholder, variable=dropdown_var, values=values)
+    dropdown.pack(fill='x', expand=True)
+    dropdown_var.trace('w', lambda *args: on_dropdown_select())
 
 # Create tab frames
 tab1_frame = ctk.CTkFrame(root)
@@ -70,12 +74,19 @@ tab2_button = ctk.CTkButton(tabs_container, text="Registru Rebut", command=lambd
 tab1_button.pack(side='left', fill='x', expand=True)
 tab2_button.pack(side='left', fill='x', expand=True)
 
+
 # Add Open Excel button to Tab 1
 open_excel_button = ctk.CTkButton(tab1_frame, text="Open Excel File", command=open_excel_file)
 open_excel_button.pack(pady=10)
 
-# Create initial empty dropdown in Tab 1
-create_dropdown([])
+
+# Placeholder frame for dropdown in Tab 1
+dropdown_placeholder = ctk.CTkFrame(tab1_frame)
+dropdown_placeholder.pack(pady=10)
+
+
+# Create initial empty dropdown in Tab 1 with a placeholder value
+create_dropdown(["Select Tambur"])
 
 # Create Treeview in Tab 1
 tree_columns = ("Tambur", "KG/Rola", "Nr.InternRola")
